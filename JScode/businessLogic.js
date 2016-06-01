45e9c7152d7de97b3ff0dc599be3ba0f948e067e
Boris Larchenko <borislarchencko@gmail.com>
2016-06-01 21:05:03 +0300

    slider: second edition

--- JScode/businessLogic.js
+++ JScode/businessLogic.js
@@ -1,3 +1,36 @@
 /**
  * Created by boris on 6/1/16.
  */
+
+function HelloFunction() {
+    if (i == 0){
+        i = 29;
+    }
+    else {
+    i = i - 1;
+var aplm = respon.photos.photo[i].url_m;
+var image=document.getElementById('change');
+image.src = aplm;
+    }
+    
+}
+
+function RightFunc() {
+   if (i == 29){
+       i = 0;
+   }
+    else {
+           i = i + 1;
+var aplm = respon.photos.photo[i].url_m;
+var image=document.getElementById('change');
+image.src = aplm;
+       
+   }
+
+
+}
+var i = 10;
+var aplm = respon.photos.photo[i].url_m;
+var image=document.getElementById('change');
+image.src = aplm;
+
